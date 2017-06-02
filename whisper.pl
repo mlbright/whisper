d7a875b3c21a61899927ef8d55418864a09ddd66
Martin-Louis Bright <martin.louis.bright@autodesk.com>
2017-06-02 12:01:09 -0400

    personal secret server

--- whisper.pl
+++ whisper.pl
@@ -2,22 +2,24 @@
 
 use Mojolicious::Lite;
 
-state $password = "";
-
-post '/' => sub {
-    my $c = shift;
-    $password = $c->req->body;
-    $c->render( text => "thank you" );
+helper secrets => sub {
+    state $cache = {};
 };
 
-get '/' => sub {
-    my $c         = shift;
-    my $remote_ip = $c->tx->remote_address;
-    if ( $remote_ip eq "127.0.0.1" ) {
-        $c->render( text => $password );
+post '/:secret' => sub {
+    my $c = shift;
+    $c->secrets->{ $c->stash('secret') } = $c->req->body;
+    $c->render( text => "thank you!" );
+};
+
+get '/:secret' => sub {
+    my $c = shift;
+    if ( $c->tx->remote_address eq "127.0.0.1" ) {
+        $c->render( text => $c->secrets->{ $c->stash('secret') } );
     }
     else {
-        $c->render( text => "Stop snooping! request from $remote_ip" );
+        $c->render( text =>
+              "Stop snooping! Rejecting requests from $c->tx->remote_address" );
     }
 };
 
